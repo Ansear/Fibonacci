1cf0ece1ce969869e40450e41d903414b7ba7487
Ansear <sebastianarciniegas2003@gmail.com>
2023-12-04 18:00:40 -0500

    feat: :tada: Init Repository

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.Clear();
+        Console.WriteLine("Seleccione el programa \n 1.Programa 1 \n 2.Programa 2 \n 3.Programa 3");
+        int opc = Convert.ToInt16(Console.ReadLine());
+        switch (opc)
+        {
+            case 1:
+                Console.WriteLine("Ingrese n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int j = 0;
+                int k = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int c = k;
+                    k += j;
+                    j = c;
+                }
+                Console.WriteLine($"F{n} = {k}");
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Ingrese un numero: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                int a = 0;
+                int b = 1;
+
+                for (int i = 2; i <= num; i++)
+                {
+                    int c = b;
+                    b += a;
+                    a = c;
+                    if (b == num)
+                    {
+                        Console.WriteLine($"{num} Es un numero de Fibonacci");
+                        break;
+                    }
+                    else if (b > num)
+                    {
+                        Console.WriteLine($"{num} No es un numero de Fibonacci");
+                        break;
+                    }
+                }
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("Ingrese un numero: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                int d = 0;
+                int e = 1;
+                for (int i = 0; i <= m; i++)
+                {
+                    if (i == m)
+                        break;
+                    Console.Write($" {d}");
+                    int f = e;
+                    e += d;
+                    d = f;
+                }
+                break;
+            default:
+                Console.WriteLine("Valor Ingresado invalido");
+
+                break;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Fibonacci 
+- Se realizo el ejercicio de la secuencia Fibonacci, se tiene un menu para elegir el programa requerido
+
+## Programa 1
+- Escriba un programa que reciba como entrada un número entero n, y entregue como salida el n-
+ésimo número de Fibonacci.
+
+## Programa 2
+- Escriba un programa que reciba como entrada un número entero e indique si es o no un número
+de Fibonacci.
+
+## Programa 3 
+- Escriba un programa que muestres los m primeros números de Fibonacci, donde m es un número
+ingresado por el usuario.
